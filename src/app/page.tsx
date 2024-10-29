c78fb349182cf599c63170d84f66c88b8413ba64
Willer Lucoles <132530244+WillerLucoles@users.noreply.github.com>
2024-10-29 16:40:51 -0300

    page.tsx
    Reposição dos components com bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 
-
+import About from "./components/about";
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Steps from "./components/steps";
+import TestimonialsSection from "./components/testimonials";
+import Works from "./components/works";
+import Status from "./components/status.";
+import News from "./components/news";
+import ContactUS from "./components/contactUs";
+import Footer from "./components/footer";
 
 
 
